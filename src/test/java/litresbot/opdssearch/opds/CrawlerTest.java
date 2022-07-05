e67282fefbd408720f9a900cd13192598e09951b
anechka-k <anechka.k123@yandex.com>
2022-07-06 01:15:55 +0200

    Additional Crawler tests

--- src/test/java/litresbot/opdssearch/opds/CrawlerTest.java
+++ src/test/java/litresbot/opdssearch/opds/CrawlerTest.java
@@ -10,7 +10,26 @@ import static org.mockito.Mockito.*;
 import litresbot.http.HttpClientWithProxy;
 
 public class CrawlerTest {
-  String searchResult =
+  String searchResultPage1 =
+    "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n <feed xmlns=\"http://www.w3.org/2005/Atom\" xmlns:dc=\"http://purl.org/dc/terms/\" xmlns:os=\"http://a9.com/-/spec/opensearch/1.1/\" xmlns:opds=\"http://op" +
+    "ds-spec.org/2010/catalog\"> <id>tag:search:books:книга:</id>\n <title>Результат поиска</title>\n <updated>2022-07-06T00:43:41+02:00</updated>\n <icon>/favicon.ico</icon>\n <link href=\"/opds-opensearc" +
+    "h.xml\" rel=\"search\" type=\"application/opensearchdescription+xml\" />\n <link href=\"/opds/search?searchTerm={searchTerms}\" rel=\"search\" type=\"application/atom+xml\" />\n <link href=\"/opds\" r" +
+    "el=\"start\" type=\"application/atom+xml;profile=opds-catalog\" />\n <link href=\"/opds/search?searchTerm=%D0%BA%D0%BD%D0%B8%D0%B3%D0%B0\" rel=\"up\" type=\"application/atom+xml;profile=opds-catalog\"" +
+    " />\n <link href=\"/opds/search?searchType=books&amp;searchTerm=%D0%BA%D0%BD%D0%B8%D0%B3%D0%B0&amp;pageNumber=1\" rel=\"next\" type=\"application/atom+xml;profile=opds-catalog\" />\n <entry> <updated>" +
+    "2022-07-06T00:43:41+02:00</updated>\n <title>   Книга о слонах</title>\n <author> <name>Бауэр Ганс</name>\n <uri>/a/127691</uri>\n</author>\n <link href=\"/opds/author/127691\" rel=\"related\" type=\"" +
+    "application/atom+xml\" title=\"Все книги автора Бауэр Ганс\" />\n <category term=\"Зоология\" label=\"Зоология\" />\n <dc:language>ru</dc:language>\n <dc:format>pdf</dc:format>\n <dc:issued>1964</dc:i" +
+    "ssued>\n <content type=\"text/html\">Эта книга об одних из наиболее интересных и популярных животных земного шара. Ее автор — немецкий писатель Ганс Бауэр  стремился сообщить о них самые разнообразные" +
+    " сведения. Живо и увлекательно рассказывает он о жизни слонов среди природы и в неволе в давно минувшие и нынешние времена, об удивительных физиологических особенностях этих гигантских представителей " +
+    "животного мира, о том, как ловят слонов и как их приручают и дрессируют, о ценнейших слоновых бивнях — слоновой кости и о многих других интереснейших особенностях этих своеобразных животных.\n\n«Мысль" +
+    "»,1964. — 175 с.&lt;br/&gt;Год издания: 1964&lt;br/&gt;Формат: pdf&lt;br/&gt;Язык: ru&lt;br/&gt;Размер: 9020 Kb&lt;br/&gt;Скачиваний: 497&lt;br/&gt;</content>\n <link href=\"/b/350913/download\" rel=\"" +
+    "http://opds-spec.org/acquisition/open-access\" type=\"application/pdf\" />\n <link href=\"/b/350913\" rel=\"alternate\" type=\"text/html\" title=\"Книга на сайте\" />\n <id>tag:book:ae76ead09d3b2e9d4" +
+    "d96ffb555a26898</id>\n</entry>\n <entry> <updated>2022-07-06T00:43:41+02:00</updated>\n <title> Путь в беспредельность (книга 4)</title>\n <author> <name>Быстров Николай Семенович</name>\n <uri>/a/161" +
+    "999</uri>\n</author>\n <link href=\"/opds/author/161999\" rel=\"related\" type=\"application/atom+xml\" title=\"Все книги автора Быстров Николай Семенович\" />\n <category term=\"Неотсортированное\" l" +
+    "abel=\"Неотсортированное\" />\n <dc:language>ru</dc:language>\n <dc:format>pdf</dc:format>\n <dc:issued>2015</dc:issued>\n <content type=\"text/html\">Год издания: 2015&lt;br/&gt;Формат: pdf&lt;br/&gt" +
+    ";Язык: ru&lt;br/&gt;Размер: 825 Kb&lt;br/&gt;Скачиваний: 156&lt;br/&gt;</content>\n <link href=\"/b/435470/download\" rel=\"http://opds-spec.org/acquisition/open-access\" type=\"application/pdf\" />\n" +
+    " <link href=\"/b/435470\" rel=\"alternate\" type=\"text/html\" title=\"Книга на сайте\" />\n <id>tag:book:1d059f06f8c57537d40ec8816a0549f3</id>\n</entry>\n</feed>\n";
+
+  String searchResultPage2 =
     "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n <feed xmlns=\"http://www.w3.org/2005/Atom\" xmlns:dc=\"http://purl.org/dc/terms/\" xmlns:os=\"http://a9.com/-/spec/opensearch/1.1/\" xmlns:opds=\"http://op" +
     "ds-spec.org/2010/catalog\"> <id>tag:search:books:тиньков:</id>\n <title>Результат поиска</title>\n <updated>2022-07-03T01:14:36+02:00</updated>\n <icon>/favicon.ico</icon>\n <link href=\"/opds-opensea" +
     "rch.xml\" rel=\"search\" type=\"application/opensearchdescription+xml\" />\n <link href=\"/opds/search?searchTerm={searchTerms}\" rel=\"search\" type=\"application/atom+xml\" />\n <link href=\"/opds\"" +
@@ -47,7 +66,7 @@ public class CrawlerTest {
   @Test 
   public void testDownloadPageOk() throws IOException {
     HttpClientWithProxy mockHttp = mock(HttpClientWithProxy.class);
-    when(mockHttp.sendGetRequest(anyString())).thenReturn(searchResult);
+    when(mockHttp.sendGetRequest(anyString())).thenReturn(searchResultPage2);
     Crawler crawler = new Crawler(mockHttp);
     Page page = crawler.downloadPage("url");
     Assert.assertNull(page.title); 
@@ -89,17 +108,55 @@ public class CrawlerTest {
   @Test 
   public void testDownloadCatalogOk() throws IOException {
     HttpClientWithProxy mockHttp = mock(HttpClientWithProxy.class);
-    when(mockHttp.sendGetRequest(anyString())).thenReturn(searchResult);
+    when(mockHttp.sendGetRequest(anyString())).thenReturn(searchResultPage1).thenReturn(searchResultPage2);
+    Crawler crawler = new Crawler(mockHttp);
+    List<Page> pages = crawler.downloadCatalog("root", "url");
+    Assert.assertEquals(2, pages.size());
+    Assert.assertNull(pages.get(0).title);
+    Assert.assertEquals(2, pages.get(0).entries.size());
+    Assert.assertEquals(5, pages.get(0).links.size());
+    Assert.assertNull(pages.get(1).title);
+    Assert.assertEquals(2, pages.get(1).entries.size());
+    Assert.assertEquals(4, pages.get(1).links.size());
+  }
+
+  @Test 
+  public void testDownloadCatalogCyclicFirst() throws IOException {
+    HttpClientWithProxy mockHttp = mock(HttpClientWithProxy.class);
+    when(mockHttp.sendGetRequest(anyString())).thenReturn(searchResultPage1).thenReturn(searchResultPage1).thenReturn(searchResultPage1);
+    Crawler crawler = new Crawler(mockHttp);
+    List<Page> pages = crawler.downloadCatalog("root", "/opds/search?searchType=books&searchTerm=%D0%BA%D0%BD%D0%B8%D0%B3%D0%B0&pageNumber=1");
+    Assert.assertEquals(1, pages.size());
+    Assert.assertNull(pages.get(0).title);
+    Assert.assertEquals(2, pages.get(0).entries.size());
+    Assert.assertEquals(5, pages.get(0).links.size());
+  }
+
+  @Test 
+  public void testDownloadCatalogCyclicNext() throws IOException {
+    HttpClientWithProxy mockHttp = mock(HttpClientWithProxy.class);
+    when(mockHttp.sendGetRequest(anyString())).thenReturn(searchResultPage1).thenReturn(searchResultPage1).thenReturn(searchResultPage1);
+    Crawler crawler = new Crawler(mockHttp);
+    List<Page> pages = crawler.downloadCatalog("root", "url");
+    Assert.assertEquals(2, pages.size());
+    Assert.assertNull(pages.get(0).title);
+    Assert.assertEquals(2, pages.get(0).entries.size());
+    Assert.assertEquals(5, pages.get(0).links.size());
+    Assert.assertNull(pages.get(1).title);
+    Assert.assertEquals(2, pages.get(1).entries.size());
+    Assert.assertEquals(5, pages.get(1).links.size());
+  }
+
+  @Test 
+  public void testDownloadCatalogBad() throws IOException {
+    String badResult = "<?bad";
+    HttpClientWithProxy mockHttp = mock(HttpClientWithProxy.class);
+    when(mockHttp.sendGetRequest(anyString())).thenReturn(badResult);
     Crawler crawler = new Crawler(mockHttp);
     List<Page> pages = crawler.downloadCatalog("root", "url");
     Assert.assertEquals(1, pages.size());
     Assert.assertNull(pages.get(0).title);
-    Assert.assertEquals(2, pages.get(0).entries.size());
-    Assert.assertEquals(4, pages.get(0).links.size());
+    Assert.assertEquals(0, pages.get(0).entries.size());
+    Assert.assertEquals(0, pages.get(0).links.size());
   }
-
-  // OPDS will be deprecated so:
-  // no test for iterating over catalog pages
-  // no test for cyclic links
-  // no test for bad catalog data
 }
